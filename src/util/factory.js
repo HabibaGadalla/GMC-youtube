8379ada4c320a7e9c3e2d95438f25213a6854c64
HabibaGadalla <habiba.m.g@gmail.com>
2021-04-16 13:34:31 +0200

    chore: video factory

---
+++ src/util/factory.js
@@ -0,0 +1,36 @@
+
+export const videoFactory = [
+    {
+      videoSrc: "/media/cc0-videos/flower.mp4",
+      videoTitle: "Amreen - Amr Diab",
+      videoArtist: "Amr Diab",
+      videoViews: 1000000,
+      videoDate: new Date(),
+      videoThumbnail:
+        "https://cdn.al-ain.com/images/2020/10/06/127-234238-amr-diab-beauties_700x400.jpg",
+      isPlaying: false,
+      videoId: 1,
+    },
+    {
+      videoSrc: "/media/cc0-videos/flower.mp4",
+      videoTitle: "We are Good - Dua Lipa",
+      videoArtist: "Dua Lipa",
+      videoViews: 3000000,
+      videoDate: new Date(),
+      videoThumbnail:
+        "https://sneakerspirit.com/fr/wp-content/uploads/sites/3/2021/02/manchester-arena-2020-CD1-dua-lipa-DAIMANI-VIP-Tickets.jpg",
+      isPlaying: true,
+      videoId: 9,
+    },
+    {
+      videoSrc: "/media/cc0-videos/flower.mp4",
+      videoTitle: "Marwan Pablo - Ghaba",
+      videoArtist: "Marwan Pablo",
+      videoViews: 10000000,
+      videoDate: new Date(),
+      videoThumbnail:
+        "https://scenenoise.com/Content/Articles/Big_image/637200954689447468_ScreenShot2020-03-18at11.30.44AM.png",
+      isPlaying: true,
+      videoId: 19,
+    },
+  ];
